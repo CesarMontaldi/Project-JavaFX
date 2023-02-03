9a75772b3bc24dbfeaddd566819820e58b3dfb64
Cesar <guto_montaldi@yahoo.com.br>
2023-02-03 16:31:10 -0300

    DepartmentService

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -15,6 +15,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
 
 public class MainViewController implements Initializable {
 
@@ -34,7 +35,7 @@ public class MainViewController implements Initializable {
 	
 	@FXML 
 	public void onMenuItemDepartmentAction() {
-		loadView("/gui/DepartmentList.fxml");
+		loadView2("/gui/DepartmentList.fxml");
 	}
 	
 	@FXML 
@@ -58,6 +59,28 @@ public class MainViewController implements Initializable {
 			mainVBox.getChildren().clear();// # Limpa todos os filhos do VBox da tela principal.
 			mainVBox.getChildren().add(mainMenu);// # Adiciona mainMenu da tela principal de volta.
 			mainVBox.getChildren().addAll(newVBox.getChildren());// # Adiciona o newVBox e os seus filhos da tela about.
+		}
+		catch (IOException e) {
+			Alerts.showAlert("IOEexception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+	private synchronized void loadView2(String absoluteName) { // # Asegura que a aplicação rode sem sofre alguma interrupção pelo multithreds.
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();// # Cria uma referencia para a Scene principal que esta em Main.java.
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();// # Cria uma referencia para a VBox.
+			
+			Node mainMenu = mainVBox.getChildren().get(0);// # Cria uma referencia para o primeiro filho do VBox que é o mainMenu da tela principal.
+			mainVBox.getChildren().clear();// # Limpa todos os filhos do VBox da tela principal.
+			mainVBox.getChildren().add(mainMenu);// # Adiciona mainMenu da tela principal de volta.
+			mainVBox.getChildren().addAll(newVBox.getChildren());// # Adiciona o newVBox e os seus filhos da tela about.
+			
+			DepartmentListController controller = loader.getController();
+			controller.SetDepartmentService(new DepartmentService());
+			controller.updateTableView();
 		}
 		catch (IOException e) {
 			Alerts.showAlert("IOEexception", "Error loading view", e.getMessage(), AlertType.ERROR);
